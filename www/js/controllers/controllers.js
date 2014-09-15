5897e40414bc033d46944349fabb2990f71b70d0
perrins <matthewperrins@gmail.com>
2014-09-15 14:50:03 +0100

    New Changes

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -13,6 +13,8 @@ angular.module('gameboard.controllers', [])
     // Prepare User for Display
     if ($rootScope.user) {
 
+
+
         $scope.user = $rootScope.user;
         $scope.member = $rootScope.member;
 
@@ -104,6 +106,7 @@ angular.module('gameboard.controllers', [])
 
                             $ionicLoading.hide();
                             $rootScope.user.registered = false;
+                            $rootScope.user.avatar = "img/avatar.png";
                             $state.go('intro');
                         });
 
@@ -131,16 +134,13 @@ angular.module('gameboard.controllers', [])
 // A simple controller that shows a tapped item's data
 .controller('RegisterCtrl', function($ionicScrollDelegate, $rootScope, $state, $scope, MembersService, WizardHandler,$ionicPopup) {
 
+    // Check if user is defined
+    if (!rootScope.user){
+        $state.go("signin");
+    }
+
     // Manage the Registration Process
     $scope.user = $rootScope.user;
-
-    // Test Data
-    $scope.member = {
-
-        muuid: $scope.user.raw.id,
-        user: $scope.user
-
-    };
 
     // Move the Name section
     $scope.next = function() {
@@ -214,15 +214,17 @@ angular.module('gameboard.controllers', [])
     // Manage the Registration Process
     var user = $rootScope.user;
 
-    // If they are not registered then take them to registration
-    if (!$scope.user.registered) {
-        $state.go('register');
-        return;
-    }
+    debugger;
 
     // No User lets navigate
     if(!user) {
-        $state.go('intro')
+        $state.go('signin');
+        return;
+    }
+
+    // If they are not registered then take them to registration
+    if (!user.registered) {
+        $state.go('register');
         return;
     }
 
